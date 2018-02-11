375bfde741b515faaf85e47582332bff9f61af4b
Cheng <allen.cheng@delphi.com>
2018-02-11 09:20:13 +0800

    commit

--- main.rxjs.js
+++ main.rxjs.js
@@ -1,18 +1,62 @@
 let Rx = require('rxjs');
 
-function observer(name) {
-    return {
-        next: (value) => console.log(`observer ${name}: ${value}`),
-        complete: () => console.log(`observer ${name}: complete`)
-    };
-}
+Rx.Observable.from([0,1,2,3,4,5])
+    .switchMap(value => {
 
-const source = Rx.Observable.interval(2000).startWith(-1).do((v)=>console.log(`emmitted ${v}`));
+        // This is the disposable stream!
+        // Errors can safely occur in here without killing the original stream
 
-const p = source.multicast(new Rx.BehaviorSubject()).refCount();
+        return Rx.Observable.of(value)
+            .map(value => {
+                if (value === 3) {
+                    throw new Error('Value cannot be 3');
+                }
+                return value;
+            })
+            .catch(error => {
+                // You can do some fancy stuff here with errors if you like
+                // Below we are just returning the error object to the outer stream
+                return Rx.Observable.of(error);
+            });
 
-p.subscribe(observer("a"));
-p.subscribe(observer("b"));
-setTimeout(() => {
-    p.subscribe(observer("c"));
-}, 10);
+    })
+    .map(value => {
+        if (value instanceof Error) {
+            // Maybe do some error handling here
+            return `Error: ${value.message}`;
+        }
+        return value;
+    })
+    .subscribe(
+      (x => console.log('Success', x)),
+      (x => console.log('Error', x)),
+      (() => console.log('Complete'))
+    );
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let o = Rx.Observable.create(function subscriber(subscriber) {
+//     subscriber.next(1);
+//     subscriber.next(2);
+// })
+
+// o.switchMap((value) => {
+//     console.log(`first ${value}`);
+//     return Rx.Observable.of(...arr)
+//     .map((value) => {
+//         //console.log(value);
+//         if (value == 5)
+//             throw Error('error');
+//         else
+//             return value * 2
+//     })
+//     .catch(err => {
+//        return Rx.Observable.of(err);
+//     })})
+//     .subscribe((value) => {
+//         console.log(value);
+//     },
+//         (err) => {
+//             console.log(err)
+//         }
+//     );
